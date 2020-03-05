8755bd8800ee36956ac2f054909c174c6239f146
Jonathan Roelofs <jroelofs@jroelofs.com>
2020-03-05 16:02:25 -0700

    Add simple unittest suite

---
+++ unittests/UnitEval.cpp
@@ -0,0 +1,57 @@
+#include "gtest/gtest.h"
+
+#include "calc/Lexer.h"
+#include "calc/Parser.h"
+
+using namespace calc;
+
+TEST(Eval, MockLexer1) {
+  // clang-format off
+  VectorLexer L({
+    Token(Token::Number, "1"),
+    Token::Plus,
+    Token(Token::Number, "2")
+  });
+  // clang-format on
+
+  Parser<int> P(L);
+  EXPECT_EQ(P.parseExpr(), 3);
+}
+
+TEST(Eval, MockLexer2) {
+  // clang-format off
+  VectorLexer L({
+    Token(Token::Number, "1"),
+    Token::Times,
+    Token(Token::Number, "2"),
+    Token::Plus,
+    Token(Token::Number, "3")
+  });
+  // clang-format on
+
+  Parser<int> P(L);
+  EXPECT_EQ(P.parseExpr(), 5);
+}
+
+TEST(Eval, Expressions) {
+  // clang-format off
+  struct {
+    const char *input;
+    int expected;
+  } vec[] = {
+    {"1", 1},
+    {"(1)", 1}
+    {"1+1", 2},
+    {"1+2*3", 7},
+    {"2*(1+1)", 4},
+    {"1 + 1", 2}
+  };
+  // clang-format on
+
+  for (const auto &v : vec) {
+    std::stringstream SS(v.input);
+    IOSLexer L(SS);
+    Parser<int> P(L);
+    EXPECT_EQ(P.parseExpr(), v.expected) << v.input;
+  }
+}
